c0893688c13c7557e83003ac927f8b34c3794f18
Yale Araujo <yale850@gmail.com>
2025-06-01 12:44:25 -0300

    feat: wrapper and add-new-task-form inserted into

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { AddNewTaskForm } from "./components/add-new-task-form";
 import { Header } from "./components/header";
+import { useState } from "react";
+import styles from './app.module.css'
 
 export const App = () => {
+  const [tasks, setTasks] = useState([''])
+
+  function handleAddNewTask(newTask: string){
+    setTasks(state => [...state, newTask])
+  }
+
   return (
     <main>
       <Header />
+      
+      <section className={styles.wrapper}>
+        <AddNewTaskForm onSubmit={handleAddNewTask}/>
+      </section>
     </main>
   );
 };
