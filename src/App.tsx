66fa6c9cafbb8513aef6b681ed461ea1b484119a
Yale Araujo <yale850@gmail.com>
2025-06-02 01:15:50 -0300

    chore: added new component and applied linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
+import styles from "./app.module.css";
+
 import { AddNewTaskForm } from "./components/add-new-task-form";
 import { Header } from "./components/header";
 import { useState } from "react";
-import styles from './app.module.css'
+import { TaskListHeader } from "./components/task-list/task-list-header";
 
 export const App = () => {
-  const [tasks, setTasks] = useState([''])
+  const [tasks, setTasks] = useState([""]);
 
-  function handleAddNewTask(newTask: string){
-    setTasks(state => [...state, newTask])
+  function handleAddNewTask(newTask: string) {
+    setTasks((state) => [...state, newTask]);
   }
 
   return (
     <main>
       <Header />
-      
+
       <section className={styles.wrapper}>
-        <AddNewTaskForm onSubmit={handleAddNewTask}/>
+        <AddNewTaskForm onSubmit={handleAddNewTask} />
+
+        <div className={styles.taskList}>
+          <TaskListHeader tasksCount={5} doneTasksCount={2} />
+        </div>
       </section>
     </main>
   );
